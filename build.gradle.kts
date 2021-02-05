45b2e15ae6ba310ce0ec4f4b506c5c18e8c5ce90
theapache64 <theapache64@gmail.com>
2021-02-06 01:11:31 +0530

    🌛 first commit

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.30"
+    id("org.jetbrains.compose") version "0.3.0-build149"
+}
+
+group = "com.theapache64"
+version = "1.0.0-alpha01"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+    implementation(compose.desktop.currentOs)
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "stackzy"
+        }
+    }
+}
