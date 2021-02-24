f377c8e8ec10eb33b432610a9a157b1cec4ba7e3
theapache64 <theapache64@gmail.com>
2021-02-24 23:35:41 +0530

    © change term 'OPEN MARKET' to 'SEARCH IN MARKET'

--- src/main/kotlin/com/theapache64/stackzy/util/R.kt
+++ src/main/kotlin/com/theapache64/stackzy/util/R.kt
@@ -3,7 +3,7 @@ package com.theapache64.stackzy.util
 object R {
     @Suppress("ClassName")
     object string {
-        const val app_detail_action_open_market = "OPEN MARKET"
+        const val app_detail_action_open_market = "SEARCH IN MARKET"
         const val any_error_title_damn_it: String = "Damn It!"
         const val app_detail_loading_fetching_apk: String = "Fetching APK..."
         const val app_detail_loading_decompiling: String = "Decompiling..."
