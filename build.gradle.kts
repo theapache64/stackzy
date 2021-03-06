9ac4ac702a50c499e39f0a66454cee04a44e1366
theapache64 <theapache64@gmail.com>
2021-03-06 20:24:02 +0530

    ➕ add uuid for windows

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,6 +108,8 @@ compose.desktop {
 
             windows {
                 iconFile.set(iconsRoot.resolve("launcher_icons/windows.ico"))
+                upgradeUuid = "31575EDF-D0D5-4CEF-A4D2-7562083D6D88"
+                menuGroup = packageName
             }
 
             macOS {
