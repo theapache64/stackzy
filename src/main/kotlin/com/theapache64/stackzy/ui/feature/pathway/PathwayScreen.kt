9d5a850a0db9197c575a6cdfab4bb4e52e64c866
theapache64 <theapache64@gmail.com>
2021-03-09 10:40:20 +0530

    ➕ add image to pathway card

--- src/main/kotlin/com/theapache64/stackzy/ui/feature/pathway/PathwayScreen.kt
+++ src/main/kotlin/com/theapache64/stackzy/ui/feature/pathway/PathwayScreen.kt
@@ -1,14 +1,17 @@
 package com.theapache64.stackzy.ui.feature.pathway
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.svgResource
 import androidx.compose.ui.unit.dp
-import com.theapache64.stackzy.ui.theme.R
 import com.theapache64.stackzy.ui.util.Preview
 
 fun main(args: Array<String>) {
@@ -43,9 +46,22 @@ fun PathwayCard(
 ) {
     Column(
         modifier = Modifier
-            .width(300.dp)
-            .background(R.color.BigStone)
+            .width(200.dp)
+            .fillMaxHeight(0.5f)
+            .background(MaterialTheme.colors.secondary, RoundedCornerShape(10.dp)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+        /*Icon*/
+        Image(
+            painter = icon,
+            modifier = Modifier.size(80.dp),
+            contentDescription = "play store"
+        )
+
+        Text(
+            text = text
+        )
     }
 }
