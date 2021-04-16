0cc36098c83207393aeca0ec36d45602a96d2c9b
theapache64 <theapache64@gmail.com>
2021-04-16 22:45:49 +0530

    🔖 start next version development (v1.0.2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 val daggerVersion by extra("2.31.2")
-val stackzyVersion by extra("1.0.1")
+val stackzyVersion by extra("1.0.2")
 
 group = "com.theapache64"
 version = stackzyVersion // TODO : Change in App.kt also
