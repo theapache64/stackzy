ee4cd57ea262fcfffbcf08585f5c702700e7575c
theapache64 <theapache64@users.noreply.github.com>
2022-01-22 18:35:31 +0530

    ⬆️ bump app version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 val daggerVersion by extra("2.40.5")
-val stackzyVersion by extra("1.2.0") // TODO : Change in App.kt also
+val stackzyVersion by extra("1.2.1") // TODO : Change in App.kt also
 
 group = "com.theapache64"
 version = stackzyVersion
